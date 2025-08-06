6358518777e9008682427ca8ab8a5668dfc69381
SaloniParmar-tech <parmarsaloni544@gmail.com>
2025-08-06 18:17:28 +0530

    exploring get,post,delete

---
+++ Readme.md
@@ -0,0 +1,15 @@
+create a repository
+Initialize the repository
+Install express
+Create a server
+Write request handler for 
+Install nodemon and update scripts inside package.json
+
+initialize git
+.gitignore
+Create a remote repo on github
+push all code to remote origin
+*order of the routes matter a lot
+browser is not good for api testing so we use postman
+install postman app and  make a workspace/collection > test api call
+
--- src/app.js
+++ src/app.js
@@ -1,8 +1,29 @@
 // console.log("HEllo");
 const express = require("express");
 const app = express();
-app.use((req,res) =>{//REQUEST HANDLER
-    res.send("HELLO FROM THE SERVER TO THE  USER....");
+// app.use((req,res) =>{//REQUEST HANDLER
+//     res.send("HELLO FROM THE SERVER TO THE  USER....");
+// });
+
+//this will excutes first and matches with all the api so for every api it excutes
+// app.use("/hello",(req,res) =>{//REQUEST HANDLER
+//     res.send("HELLO and Namaste");
+// });
+
+app.get("/user",(req,res) =>{//REQUEST HANDLER
+    res.send({firstname:"Saloni", lastname:"Parmar"});
+});
+
+app.post("/user",(req,res) =>{//REQUEST HANDLER
+    res.send("HELLO , Data successfully saved to the database");
+});
+
+app.delete("/user",(req,res) =>{//REQUEST HANDLER
+    res.send("HELLO, Deleted Successfully");
+});
+
+app.use("/hello",(req,res) =>{//REQUEST HANDLER
+    res.send("HELLO and Namaste");
 });
 
 app.listen(3000,()=>{
